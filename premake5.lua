f9c1b7b9ca0a8f8e1de6165e4e74e29acb9cfadc
Logstor <a.rottger_rydahl@live.com>
2020-04-18 18:15:39 +0200

    Premake file created and working.

---
+++ premake5.lua
@@ -0,0 +1,104 @@
+workspace "Asaurus"
+	architecture "x64"
+	configurations
+	{
+		"Debug", 
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Asaurus"
+	location "Asaurus"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{ 
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines
+		{
+			"AS_PLATFORM_WINDOWS",
+			"AS_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines { "AS_DEBUG", "DEBUG" }
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines { "AS_RELEASE", "RELEASE" }
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines { "AS_DIST", "RELEASE" }
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{ 
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Asaurus/vendor/spdlog/include",
+		"Asaurus/src"
+	}
+
+	links
+	{
+		"Asaurus"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines
+		{
+			"AS_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines { "AS_DEBUG", "DEBUG" }
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines { "AS_RELEASE", "RELEASE" }
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines { "AS_DIST", "RELEASE" }
+		optimize "On"
