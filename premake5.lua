4cc8814515547a9f697f2e48538fa073a44720fa
Logstor <a.rottger_rydahl@live.dk>
2020-04-24 20:49:49 +0200

    Added Math library glm as submodule + Update premake.

--- premake5.lua
+++ premake5.lua
@@ -16,6 +16,7 @@ IncludeDir = {}
 IncludeDir["GLFW"] = "Asaurus/vendor/GLFW/include"
 IncludeDir["Glad"] = "Asaurus/vendor/Glad/include"
 IncludeDir["ImGui"] = "Asaurus/vendor/imgui"
+IncludeDir["glm"] = "Asaurus/vendor/glm"
 
 -- Include other Projects premake5.lua file
 group "Dependencies"
@@ -43,7 +44,9 @@ project "Asaurus"
 	files
 	{ 
 		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl"
 	}
 
 	includedirs
@@ -52,7 +55,8 @@ project "Asaurus"
 		"%{prj.name}/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}"
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}"
 	}
 
 	links
@@ -113,7 +117,8 @@ project "Sandbox"
 	includedirs
 	{
 		"Asaurus/vendor/spdlog/include",
-		"Asaurus/src"
+		"Asaurus/src",
+		"%{IncludeDir.glm}"
 	}
 
 	links
