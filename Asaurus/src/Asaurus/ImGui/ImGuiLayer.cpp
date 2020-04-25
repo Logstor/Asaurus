969789914ddfc07d3ad750815dca4ee261c07122
Logstor <a.rottger_rydahl@live.dk>
2020-04-25 13:02:02 +0200

    Corrected io.DisplaySize assignment to convert the unsigned ints to floats.

--- Asaurus/src/Asaurus/ImGui/ImGuiLayer.cpp
+++ Asaurus/src/Asaurus/ImGui/ImGuiLayer.cpp
@@ -71,7 +71,7 @@ namespace Asaurus
 	{
 		ImGuiIO& io = ImGui::GetIO();
 		Application& app = Application::Get();
-		io.DisplaySize = ImVec2(app.GetWindow().GetWidth(), app.GetWindow().GetHeight());
+		io.DisplaySize = ImVec2((float)app.GetWindow().GetWidth(), (float)app.GetWindow().GetHeight());
 
 		// Rendering
 		ImGui::Render();
