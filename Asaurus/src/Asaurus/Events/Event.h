0e86d467d126e43c42e024b3d3c8c077f6532dac
Logstor <a.rottger_rydahl@live.dk>
2020-06-01 13:18:38 +0200

    Avoid using std::function in event dispatcher for performance.

--- Asaurus/src/Asaurus/Events/Event.h
+++ Asaurus/src/Asaurus/Events/Event.h
@@ -58,22 +58,22 @@ namespace Asaurus
 
 	class EventDispatcher
 	{
-		template<typename T>
-		using EventFn = std::function<bool(T&)>;
 	public:
 		EventDispatcher(Event& event)
 			: m_Event(event) {}
 
-		template<typename T>
-		bool Dispatch(EventFn<T> func)
+		// F will be deduced by the compiler
+		template<typename T, typename F>
+		bool Dispatch(const F& func)
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.m_Handled = func(*(T*)&m_Event);
+				m_Event.Handled = func(static_cast<T&>(m_Event));
 				return true;
 			}
 			return false;
 		}
+
 	private:
 		Event& m_Event;
 	};
