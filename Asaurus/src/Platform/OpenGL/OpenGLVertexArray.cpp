6ac517d7da8abbb6e164cee0399f9fc2d341537f
Logstor <a.rottger_rydahl@live.dk>
2020-06-01 13:01:18 +0200

    Corrected warning in OpenGLVertexArray.cpp

--- Asaurus/src/Platform/OpenGL/OpenGLVertexArray.cpp
+++ Asaurus/src/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -80,7 +80,7 @@ namespace Asaurus
 				ShaderDataTypeToOpenGLBaseType(element.Type),
 				element.Normalized ? GL_TRUE : GL_FALSE,
 				layout.GetStride(),
-				(const void*)element.Offset);
+				(const void*)(intptr_t)element.Offset);
 
 			index++;
 		}
