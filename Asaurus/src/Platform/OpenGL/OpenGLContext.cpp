82b8b6d010531cfaba639a9576fdf7a35e3d4ee9
Logstor <a.rottger_rydahl@live.dk>
2020-07-26 13:38:53 +0200

    Added check for OpenGL version

--- Asaurus/src/Platform/OpenGL/OpenGLContext.cpp
+++ Asaurus/src/Platform/OpenGL/OpenGLContext.cpp
@@ -22,6 +22,16 @@ namespace Asaurus
 		AS_CORE_INFO("    Vendor:   {0}", glGetString(GL_VENDOR));
 		AS_CORE_INFO("    Renderer: {0}", glGetString(GL_RENDERER));
 		AS_CORE_INFO("    Version:  {0}", glGetString(GL_VERSION));
+
+		// Version check
+	#ifdef AS_ENABLE_ASSERTS
+		int versionMajor;
+		int versionMinor;
+		glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+		glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+		AS_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "Asaurus requires atleast OpenGL version 4.5!");
+	#endif
 	}
 
 	void OpenGLContext::SwapBuffers()
