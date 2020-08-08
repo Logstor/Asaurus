fbdfedcc64b6bbf1a74649da2e97abe3c4cde0e4
Logstor <a.rottger_rydahl@live.dk>
2020-08-08 18:54:14 +0200

    Intrumentation update, Thread safe and Logging safety

--- Asaurus/src/Asaurus/Debug/Instrumentor.h
+++ Asaurus/src/Asaurus/Debug/Instrumentor.h
@@ -13,7 +13,7 @@ namespace Asaurus
     {
         std::string Name;
         long long Start, End;
-        uint32_t ThreadID;
+        std::thread::id ThreadID;
     };
 
     struct InstrumentationSession
@@ -24,37 +24,58 @@ namespace Asaurus
     class Instrumentor
     {
     private:
+        std::mutex m_Mutex;
         InstrumentationSession* m_CurrentSession;
         std::ofstream m_OutputStream;
-        int m_ProfileCount;
         std::string m_Remove[2] = { "__cdecl ", "void " };
 
     public:
         Instrumentor()
-            : m_CurrentSession(nullptr), m_ProfileCount(0)
-        {
-        }
+            : m_CurrentSession(nullptr)
+        { }
 
         void BeginSession(const std::string& name, const std::string& filepath = "results.json")
         {
+            std::lock_guard lock(m_Mutex);
+            if (m_CurrentSession)
+            {
+                // If there is already a current session, then close it before beginning new one.
+                // Subsequent profiling output meant for the original session will end up in the
+                // newly opened session instead. That's better than having badly formatted
+                // profiling output.
+                if (Log::GetCoreLogger())
+                { 
+                    // Edge case: BeginSession() might be before Log::Init()
+                    AS_CORE_ERROR("Instrumentor::BeginSession('{0}') when session '{1}' already open.", name, m_CurrentSession->Name);
+                }
+                InternalEndSession();
+            }
+
             m_OutputStream.open(filepath);
-            WriteHeader();
-            m_CurrentSession = new InstrumentationSession{ name };
+            if (m_OutputStream.is_open())
+            {
+                m_CurrentSession = new InstrumentationSession({ name });
+                WriteHeader();
+            }
+            else
+            {
+                if (Log::GetCoreLogger())
+                { 
+                    // Edge case: BeginSession() might be before Log::Init()
+                    AS_CORE_ERROR("Instrumentor could not open results file '{0}'.", filepath);
+                }
+            }
         }
 
         void EndSession()
         {
-            WriteFooter();
-            m_OutputStream.close();
-            delete m_CurrentSession;
-            m_CurrentSession = nullptr;
-            m_ProfileCount = 0;
+            std::lock_guard lock(m_Mutex);
+            InternalEndSession();
         }
 
         void WriteProfile(const ProfileResult& result)
         {
-            if (m_ProfileCount++ > 0)
-                m_OutputStream << ",";
+            std::stringstream json;
 
             // Cleanse name
             std::string name = result.Name;
@@ -66,22 +87,36 @@ namespace Asaurus
                     name.erase(pos, toRemove.size());
             }
 
-            m_OutputStream << "{";
-            m_OutputStream << "\"cat\":\"function\",";
-            m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
-            m_OutputStream << "\"name\":\"" << name << "\",";
-            m_OutputStream << "\"ph\":\"X\",";
-            m_OutputStream << "\"pid\":0,";
-            m_OutputStream << "\"tid\":" << result.ThreadID << ",";
-            m_OutputStream << "\"ts\":" << result.Start;
-            m_OutputStream << "}";
+            json << ",{";
+            json << "\"cat\":\"function\",";
+            json << "\"dur\":" << (result.End - result.Start) << ',';
+            json << "\"name\":\"" << name << "\",";
+            json << "\"ph\":\"X\",";
+            json << "\"pid\":0,";
+            json << "\"tid\":" << result.ThreadID << ",";
+            json << "\"ts\":" << result.Start;
+            json << "}";
 
-            m_OutputStream.flush();
+            // Lock and write to file
+            std::lock_guard lock(m_Mutex);
+            if (m_CurrentSession)
+            {
+                m_OutputStream << json.str();
+                m_OutputStream.flush();
+            }
         }
+
+        static Instrumentor& Get()
+        {
+            static Instrumentor instance;
+            return instance;
+        }
+
+    private:
 
         void WriteHeader()
         {
-            m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
+            m_OutputStream << "{\"otherData\": {},\"traceEvents\":[{}";
             m_OutputStream.flush();
         }
 
@@ -91,10 +126,19 @@ namespace Asaurus
             m_OutputStream.flush();
         }
 
-        static Instrumentor& Get()
+        /// <summary>
+        /// You must already own lock on m_Mutex before
+        /// calling InternalEndSession()
+        /// </summary>
+        void InternalEndSession()
         {
-            static Instrumentor instance;
-            return instance;
+            if (m_CurrentSession)
+            {
+                WriteFooter();
+                m_OutputStream.close();
+                delete m_CurrentSession;
+                m_CurrentSession = nullptr;
+            }
         }
     };
 
@@ -120,11 +164,11 @@ namespace Asaurus
             long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
             long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
 
-            uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
-            Instrumentor::Get().WriteProfile({ m_Name, start, end, threadID });
+            Instrumentor::Get().WriteProfile({ m_Name, start, end, std::this_thread::get_id() });
 
             m_Stopped = true;
         }
+
     private:
         const char* m_Name;
         std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
