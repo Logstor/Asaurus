f87157d88dc475299bd2a2557d3be8d0fc86c679
Logstor <a.rottger_rydahl@live.dk>
2020-08-08 14:15:22 +0200

    Added compiler dependent macro for function signature

--- Asaurus/src/Asaurus/Core/Core.h
+++ Asaurus/src/Asaurus/Core/Core.h
@@ -57,10 +57,31 @@
 #define AS_PROFILE 1
 
 #if AS_PROFILE
+	// Resolve which function signature macro will be used. Note that this only
+	// is resolved when the (pre)compiler starts, so the syntax highlighting
+	// could mark the wrong one in your editor!
+	#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+		#define AS_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__DMC__) && (__DMC__ >= 0x810)
+		#define AS_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__FUNCSIG__)
+		#define AS_FUNC_SIG __FUNCSIG__
+	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+		#define AS_FUNC_SIG __FUNCTION__
+	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+		#define AS_FUNC_SIG __FUNC__
+	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+		#define AS_FUNC_SIG __func__
+	#elif defined(__cplusplus) && (__cplusplus >= 201103)
+		#define AS_FUNC_SIG __func__
+	#else
+		#define AS_FUNC_SIG "HZ_FUNC_SIG unknown!"
+	#endif
+
 	#define AS_PROFILE_BEGIN_SESSION(name, filepath) ::Asaurus::Instrumentor::Get().BeginSession(name, filepath)
 	#define AS_PROFILE_END_SESSION() ::Asaurus::Instrumentor::Get().EndSession()
 	#define AS_PROFILE_SCOPE(name) ::Asaurus::InstrumentationTimer timer##__LINE__(name)
-	#define AS_PROFILE_FUNCTION() AS_PROFILE_SCOPE(__FUNCSIG__)
+	#define AS_PROFILE_FUNCTION() AS_PROFILE_SCOPE(AS_FUNC_SIG)
 #else
 	#define AS_PROFILE_BEGIN_SESSION(name, filepath)
 	#define AS_PROFILE_END_SESSION()
