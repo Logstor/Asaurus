2315e7a45bf2ad9f28b6fecaf3c443e4f51ca11a
Logstor <a.rottger_rydahl@live.dk>
2020-08-08 13:33:29 +0200

    Updated Renderer2D with rotation and tintcolor

--- Asaurus/src/Asaurus/Renderer/Renderer2D.h
+++ Asaurus/src/Asaurus/Renderer/Renderer2D.h
@@ -16,7 +16,49 @@ namespace Asaurus
 		// Primitives
 		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color);
 		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color);
-		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture);
-		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture);
+		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
+		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
+		
+		/// <summary>
+		/// Draws a rotated evenly colored Quad on the screen. The rotation needs to be in radians, 
+		/// ex. glm::radians(45.0f) will rotate the quad 45 degress.
+		/// </summary>
+		/// <param name="position"></param>
+		/// <param name="size"></param>
+		/// <param name="rotation">Radians</param>
+		/// <param name="color"></param>
+		static void DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, const float rotation, const glm::vec4& color);
+
+		/// <summary>
+		/// Draws a rotated evenly colored Quad on the screen. The rotation needs to be in radians, 
+		/// ex. glm::radians(45.0f) will rotate the quad 45 degress.
+		/// </summary>
+		/// <param name="position"></param>
+		/// <param name="size"></param>
+		/// <param name="rotation">Radians</param>
+		/// <param name="color"></param>
+		static void DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, const float rotation, const glm::vec4& color);
+
+		/// <summary>
+		/// Draws a rotated texture Quad on the screen. The rotation needs to be in radians, ex. glm::radians(45.0f) will rotate the quad 
+		/// 45 degress.
+		/// </summary>
+		/// <param name="position"></param>
+		/// <param name="size"></param>
+		/// <param name="rotation">Radians</param>
+		/// <param name="color"></param>
+		/// <param name="tintColor"></param>
+		static void DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, const float rotation, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
+
+		/// <summary>
+		/// Draws a rotated texture Quad on the screen. The rotation needs to be in radians, ex. glm::radians(45.0f) will rotate the quad 
+		/// 45 degress.
+		/// </summary>
+		/// <param name="position"></param>
+		/// <param name="size"></param>
+		/// <param name="rotation">Radians</param>
+		/// <param name="color"></param>
+		/// <param name="tintColor"></param>
+		static void DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, const float rotation, const Ref<Texture2D>& texture, float tilingFactor = 1.0f, const glm::vec4& tintColor = glm::vec4(1.0f));
 	};
 }
--- Asaurus/src/Platform/OpenGL/OpenGLShader.cpp
+++ Asaurus/src/Platform/OpenGL/OpenGLShader.cpp
@@ -77,6 +77,13 @@ namespace Asaurus
 		UploadUniformInt(name, value);
 	}
 
+	void OpenGLShader::SetFloat(const std::string& name, const float value)
+	{
+		AS_PROFILE_FUNCTION();
+
+		UploadUniformFloat(name, value);
+	}
+
 	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
 	{
 		AS_PROFILE_FUNCTION();
